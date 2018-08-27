c65f0c0893d0e0ee7ced198b8601465ccc1211e0
guanshiming <guanshiming@jd.com>
2018-08-27 10:02:42 +0800

    添加google guava lib

---
+++ basic/src/main/java/ai/code/mikasa/optional/OptionalMain.java
@@ -0,0 +1,10 @@
+package ai.code.mikasa.optional;
+
+import java.util.Optional;
+
+public class OptionalMain {
+    public static void main(String[] args){
+        Optional<Object> optional = Optional.of(new Object());
+
+    }
+}
