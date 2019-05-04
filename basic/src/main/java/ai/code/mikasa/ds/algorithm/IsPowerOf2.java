090d40259a352818eff6588bdfa8a267e67ed20b
guanshiming <lenn1024@163.com>
2019-05-04 08:18:52 +0800

    判断一个数是否为2的幂次方

--- basic/src/main/java/ai/code/mikasa/ds/algorithm/IsPowerOf2.java
+++ basic/src/main/java/ai/code/mikasa/ds/algorithm/IsPowerOf2.java
@@ -1,4 +1,15 @@
 package ai.code.mikasa.ds.algorithm;
 
+/**
+ * 判断一个整数是否为2的幂次方
+ */
 public class IsPowerOf2 {
+
+    public static void main(String[] args){
+        System.out.println(isPowerOf2(66));
+    }
+
+    public static boolean isPowerOf2(int num){
+        return (num & (num - 1)) == 0;
+    }
 }
